abbcf98fd4ea0120c070e15f9c6435c7f3836ec7
Michael Utt <uttmichaelj@gmail.com>
2024-03-01 22:17:19 -0800

    Wait.. animating is kinda easy and fun

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 @export var movementSpeed = 30.0
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var sprite = $Sprite2D
+@onready var anim = $AnimationPlayer
+
+func _ready():
+	anim.play('walk')
 
 func _physics_process(_delta):
 	movement()
@@ -11,4 +16,10 @@ func movement():
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction*movementSpeed
 	move_and_slide()
+	
+	if direction.x > 0.1:
+		sprite.flip_h = true
+	elif direction.x < -0.1:
+		sprite.flip_h = false
+			
 	
--- Player/player.gd
+++ Player/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 var movementSpeed = 60.0
 
+@onready var sprite = $PlayerSprite
+@onready var walkTimer = get_node('walkTimer')
+
 func _ready():
 	pass
 
@@ -12,6 +15,21 @@ func movement():
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
 	var mov = Vector2(x_mov, y_mov)
+	
+	if mov.x > 0:
+		sprite.flip_h = true
+	elif mov.x < 0:
+		sprite.flip_h = false
+		
+	if mov != Vector2.ZERO:
+		if walkTimer.is_stopped():
+			if sprite.frame >= sprite.hframes - 1:
+				sprite.frame = 0
+			else:
+				sprite.frame += 1
+			walkTimer.start()
+				
+	
 	velocity = mov.normalized()*movementSpeed
 	move_and_slide()
 
